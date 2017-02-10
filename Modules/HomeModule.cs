9821c29aca18ef25cc598ef19adf196bcd31849e
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-10 09:37:02 -0800

    Add GET route for homepage to HomeModule.cs

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,2 +1,17 @@
 using Nancy;
 using AddressBook.Objects;
+using System.Collections.Generic;
+
+namespace AddressBook
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+      Get["/"] = _ => {
+        var allContacts = Contact.GetAll();
+        return View["index.cshtml", allContacts];
+      }
+    }
+  }
+}

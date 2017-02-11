121a0038f0fe2e14e852ea4fc4eab261f7d357a8
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-02-10 16:29:08 -0800

    Add jquery click handler to whole li item

--- Content/js/scripts.js
+++ Content/js/scripts.js
@@ -3,13 +3,17 @@
 
 //User Interface Logic
 $(function(){
-    $("#submit").click(){
-        var fields = ["contact-name", "contact-phone-number", "contact-address"];
-        for (i = 0; i < fields.length; i++) {
-            if ( !$("#" + i).val() ) {
-                $(this).parent().addClass("has-danger");
-                return false;
-            }
-        }
-    }
+    // $("#submit").click(){
+    //     var fields = ["contact-name", "contact-phone-number", "contact-address"];
+    //     for (i = 0; i < fields.length; i++) {
+    //         if ( !$("#" + i).val() ) {
+    //             $(this).parent().addClass("has-danger");
+    //             return false;
+    //         }
+    //     }
+    // }
+
+    $(".front-page li").click(function(){
+        window.location = this.firstChild.getAttribute("href");
+    })
 })
